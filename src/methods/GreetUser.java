7861f4742dc0f2d029217f5796923a2ffad17b55
bernice <bernyx.owusu@gmail.com>
2024-12-26 02:31:52 +0000

    greet user method update

--- src/methods/GreetUser.java
+++ src/methods/GreetUser.java
@@ -2,14 +2,22 @@ package methods;
 
 import java.util.Scanner;
 
+// Write a method that asks a user for their name, 
+// then another method that greets the user by name.
+
 public class GreetUser {
     public static void main(String[] args) {
+        String username = askName();
+        greetUser(username);
+
+    }
+
+    public static String askName() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your name: ");
         String username = scanner.next();
         scanner.close();
-        greetUser(username);
-
+        return username;
     }
 
     public static void greetUser(String username) {
